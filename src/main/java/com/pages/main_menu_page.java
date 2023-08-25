1ecfa424f197732f1e9a22467cbbf6f928a3bcfe
Madhavi Jadhav <madhavi.j@saviant.local>
2023-08-25 22:13:20 +0530

    updated by date 25 Aug 2023

--- src/main/java/com/pages/main_menu_page.java
+++ src/main/java/com/pages/main_menu_page.java
@@ -1,37 +1,10 @@
 package com.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class Login_Page {
-	@FindBy(xpath="//input[@id='mat-input-0']")
-	private WebElement Login_Id;
+public class main_menu_page {
 	
-	@FindBy(xpath="//input[@id='mat-input-1']")
-	private WebElement password;
-
-	@FindBy(xpath="//button[@class='mat-focus-indicator mat-raised-button mat-button-base mat-primary']")
-	private WebElement login_Btn;
-
-	@FindBy(xpath="//a[normalize-space()='Forgot Password?']")
-	private WebElement forgot_password;
-	
-	public WebElement Login_Id () {
-		return Login_Id;
-	}
-	public WebElement password () {
-		return password;
-	}
-	
-	public WebElement login_Btn () {
-		return login_Btn;
-	}
-	
-	public WebElement forgot_password () {
-		return forgot_password;
-	}
-//Main menu pages and methods
 	@FindBy(xpath="(//mat-icon[@role='img'][normalize-space()='menu'])[2]")
 	private WebElement main_menu;
 	
@@ -56,5 +29,4 @@ public class Login_Page {
 	public WebElement myProperties () {
 		return myProperties;
 	}
-
 }
--- src/test/java/com/test/TC_login_page.java
+++ src/test/java/com/test/TC_login_page.java
@@ -4,30 +4,48 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import com.pages.Login_Page;
+import com.pages.main_menu_page;
 import com.utilities.BaseClass;
 import com.utilities.Library;
 
 public class TC_login_page extends BaseClass{
 	Login_Page rent=PageFactory.initElements(driver, Login_Page.class);
 	
-	
-	
-	
-	
+//	@Test
+//	public void verify_forgotPaswordLink()
+//	{
+//		Login_Page rent=PageFactory.initElements(driver, Login_Page.class);
+//		rent.forgot_password().click();
+//	}
+//	
 	
 	@Test
-	
-	public void verify_forgotPaswordLink()
+	public void verify_login_pageAgain() throws Exception
 	{
 		Login_Page rent=PageFactory.initElements(driver, Login_Page.class);
-		rent.forgot_password().click();
-	}
-	@Test
-	public void verify_login_pageAgain()
-	{
 		
-		Library.Custom_SendKeys(rent.Login_Id(), excel.getStringData("Sheet1", 0, 1),"Email ID send successfully");
-		Library.Custom_SendKeys(rent.password(), excel.getStringData("Sheet1", 1, 1), "Password send successfully");
-		Library.Custom_Click(rent.login_Btn(), "Login button clicks successfully");
+		rent.Login_Id().sendKeys("swati_goswami@saviantconsulting.com");
+		rent.password().sendKeys("rebadev");
+		rent.login_Btn().click();
+		Thread.sleep(4000);
+		
+		rent.main_menu().click();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		Thread.sleep(2000);
+		rent.account().click();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		rent.notifications().click();
+		Thread.sleep(4000);
+		driver.navigate().refresh();
+		rent.myProperties().click();
+		
+		
+		
+//		Library.Custom_SendKeys(rent.Login_Id(), excel.getStringData("Sheet1", 0, 1),"Email ID send successfully");
+//		Library.Custom_SendKeys(rent.password(), excel.getStringData("Sheet1", 1, 1), "Password send successfully");
+//		Library.Custom_Click(rent.login_Btn(), "Login button clicks successfully");
 	}
+
 }
